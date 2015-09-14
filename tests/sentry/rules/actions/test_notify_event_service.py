076280f9bf81031d8aec8b0bd036a265f8fa2930
Ted Kaemming <ted@kaemming.com>
2015-09-14 11:58:14 -0700

    Fix `test_notify_event_service`.
    This uses a global registry that needs to be patched, as well as
    requires calling a few of methods on the plugin instance that were not
    previously mocked to ensure it is enabled before sending the
    notification.

--- tests/sentry/rules/actions/test_notify_event_service.py
+++ tests/sentry/rules/actions/test_notify_event_service.py
@@ -1,4 +1,4 @@
-from mock import MagicMock
+from mock import MagicMock, patch
 
 from sentry.testutils.cases import RuleTestCase
 from sentry.rules.actions.notify_event_service import NotifyEventServiceAction
@@ -7,16 +7,21 @@ from sentry.rules.actions.notify_event_service import NotifyEventServiceAction
 class NotifyEventServiceActionTest(RuleTestCase):
     rule_cls = NotifyEventServiceAction
 
-    def test_applies_correctly(self, mail_notify):
+    def test_applies_correctly(self):
         event = self.get_event()
 
         plugin = MagicMock()
+        plugin.is_enabled.return_value = True
+        plugin.should_notify.return_value = True
+
         rule = self.get_rule({
             'service': 'mail',
         })
-        rule.get_plugins = lambda: (plugin,)
 
-        results = list(rule.after(event=event, state=self.get_state()))
+        with patch('sentry.plugins.plugins.get') as get_plugin:
+            get_plugin.return_value = plugin
+
+            results = list(rule.after(event=event, state=self.get_state()))
 
         assert len(results) is 1
         assert plugin.should_notify.call_count is 1
